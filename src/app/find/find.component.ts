965c19ca4249a947d242c141b04a14894e65d9d2
kunal2899 <kunaljain649@gmail.com>
2021-06-30 00:54:07 +0530

    services updated

--- src/app/find/find.component.ts
+++ src/app/find/find.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-find',
@@ -8,8 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class FindComponent implements OnInit {
 
   constructor() { }
+  searchResults = true
+
+  open() {
+    $('.container').addClass('blur');
+    $('.results').addClass('blur');
+    $('.card-main').addClass('active');
+  }
 
   ngOnInit(): void {
+    $('.card-main .close').on('click', function () {
+      $('.container').removeClass('blur');
+      $('.results').removeClass('blur');
+      $('.card-main').removeClass('active');
+    })
   }
 
 }
